e6ce01833a1714121d718310137ff271d1b66ff8
Alfred <engineeralfredmuriithi@gmail.com>
2019-07-21 15:32:38 +0300

    [fix] Section links for linux

---
+++ application/views/sections/link.php
@@ -0,0 +1,2 @@
+<link type="text/css" rel="stylesheet" href="http://localhost:88/ADT/assets/cache/15609669061cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
+<script type="text/javascript" src="http://localhost:88/ADT/assets/cache/1560966905b7ad234e39508894e1b64570ab6effb5.js" charset="UTF-8"></script>
