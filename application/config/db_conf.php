db4dee79ec8e40eba3af7103df504e48b7491f32
Alfred <engineeralfredmuriithi@gmail.com>
2019-07-21 10:24:35 +0300

    [Feature] Testing Linux

--- application/config/db_conf.php
+++ application/config/db_conf.php
@@ -3,4 +3,4 @@ $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'testadt';
-$db['default']['port'] = 3307;
+$db['default']['port'] = 3306;
