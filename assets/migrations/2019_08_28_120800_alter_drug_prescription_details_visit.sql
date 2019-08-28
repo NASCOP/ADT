67d9a848f376dba0c221fd02452fd960b2023cd9
Alfred <engineeralfredmuriithi@gmail.com>
2019-08-28 07:33:28 +0300

    [Fix] Show IL pending prescriptions

---
+++ assets/migrations/2019_08_28_120800_alter_drug_prescription_details_visit.sql
@@ -0,0 +1,2 @@
+ALTER TABLE drug_prescription_details_visit CHANGE `drug_prescription_details_id` `drug_prescription_details_id` int(11) NULL//
+ALTER TABLE drug_prescription_details_visit CHANGE `visit_id` `visit_id` int(11) NULL//
