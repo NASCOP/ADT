d1fbc5a5d106245042c90ea3d8ac3fb2523b32ad
Nguru <billnguts@gmail.com>
2019-11-28 18:38:09 +0300

    [fix] sync regimen new dhis mappings

--- application/models/maps_item.php
+++ application/models/maps_item.php
@@ -35,10 +35,7 @@ class Maps_item extends Doctrine_Record {
 		return $items;
 		
 	}
-
-
-
-
+	
 	public function getItems($map) {
 		$query = Doctrine_Query::create() -> select("*") -> from("maps_item") -> where("maps_id = '$map'");
 		$items = $query -> execute(array(), Doctrine::HYDRATE_ARRAY);
